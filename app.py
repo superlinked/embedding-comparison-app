6f6154d27eb68c3764d9871c59bd27297d3b4c34
morcosharcos <mor@superlinked.com>
2024-07-10 13:36:51 +0200

    feat: finished 3d and efficiency updates

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ from omegaconf import OmegaConf, DictConfig
 
 from util.naive_embeddings import create_naive_embeddings
 from util.pca import pca_transform
-from util.visualisation import create_altair_2d_scatter
+from util.visualisation import create_altair_2d_scatter, create_plotly_3d_scatter
 from util.superlinked_embeddings import create_superlinked_embeddings
+
+
+def update_df_to_use(config):
+    pass
 
 
 def main(config: DictConfig) -> None:
@@ -21,7 +25,6 @@ def main(config: DictConfig) -> None:
     user_file = st.file_uploader(
         label="Upload the inputs to your vectors here.", type=["csv"]
     )
-
     user_input = st.text_input("Enter a URL to download data from")
 
     # Init state if not already initialized
@@ -41,6 +44,8 @@ def main(config: DictConfig) -> None:
         st.session_state["cols_to_use"] = None
     if "coloring_attribute" not in st.session_state:
         st.session_state["coloring_attribute"] = None
+    if "chart_dimension" not in st.session_state:
+        st.session_state["chart_dimension"] = None
 
     if user_file is not None:
         if user_input != "":
@@ -69,83 +74,105 @@ def main(config: DictConfig) -> None:
                 if not f == config.data.target_colname
             ],
         )
-        st.session_state["cols_to_use"] = cols_to_use
-        if st.session_state["cols_to_use"] is not None:
-            if st.button("Use selected columns!"):
-                st.session_state["df_to_use"] = st.session_state["df"].drop(
-                    [
-                        f
-                        for f in st.session_state["df"].columns
-                        if f not in cols_to_use + [config.data.target_colname]
-                    ],
-                    axis=1,
+        if st.button("Use selected columns!"):
+            st.session_state["df_to_use"] = st.session_state["df"].drop(
+                [
+                    f
+                    for f in st.session_state["df"].columns
+                    if f not in cols_to_use + [config.data.target_colname]
+                ],
+                axis=1,
+            )
+            st.session_state["naive_embeddings"] = None
+            st.session_state["superlinked_embeddings"] = None
+            st.session_state["naive_pca"] = None
+            st.session_state["superlinked_pca"] = None
+
+    if st.session_state["df_to_use"] is not None:
+        st.markdown("# Create vectors", unsafe_allow_html=True)
+        embedding_column_1, embedding_column_2 = st.columns(2)
+        with embedding_column_1:
+            st.markdown("## Create LLM embeddings")
+            if st.session_state["naive_embeddings"] is None:
+                st.session_state["naive_embeddings"] = create_naive_embeddings(
+                    st.session_state["df_to_use"], config=config
+                )
+            if st.session_state["naive_embeddings"] is not None:
+                st.write(
+                    f'LLM embeddings are created with shape: `{st.session_state["naive_embeddings"].shape}`'
                 )
 
-        if st.session_state["df_to_use"] is not None:
-            st.markdown("# Create vectors", unsafe_allow_html=True)
-            embedding_column_1, embedding_column_2 = st.columns(2)
-            with embedding_column_1:
-                st.markdown("## Create LLM embeddings")
-                if st.session_state["naive_embeddings"] is None:
-                    st.session_state["naive_embeddings"] = create_naive_embeddings(
-                        st.session_state["df_to_use"], config=config
-                    )
-                if st.session_state["naive_embeddings"] is not None:
-                    st.write(
-                        f'LLM embeddings are created with shape: `{st.session_state["naive_embeddings"].shape}`'
-                    )
-
-            with embedding_column_2:
-                st.markdown("## Create Superlinked embeddings")
-                if st.session_state["superlinked_embeddings"] is None:
-                    st.session_state[
-                        "superlinked_embeddings"
-                    ] = create_superlinked_embeddings(
-                        st.session_state["df_to_use"], config=config
-                    )
-                if st.session_state["superlinked_embeddings"] is not None:
-                    st.write(
-                        f'Superlinked embeddings are created with shape: `{st.session_state["superlinked_embeddings"].shape}`'
-                    )
-
-            st.markdown("# Visualisation")
-            if st.session_state["naive_pca"] is None:
-                st.session_state["naive_pca"]: np.ndarray = pca_transform(
-                    st.session_state["naive_embeddings"], 2
+        with embedding_column_2:
+            st.markdown("## Create Superlinked embeddings")
+            if st.session_state["superlinked_embeddings"] is None:
+                st.session_state[
+                    "superlinked_embeddings"
+                ] = create_superlinked_embeddings(
+                    st.session_state["df_to_use"], config=config
                 )
-            st.write("LLM embedding PCA transformation ready.")
-            if st.session_state["superlinked_pca"] is None:
-                st.session_state["superlinked_pca"]: np.ndarray = pca_transform(
-                    st.session_state["superlinked_embeddings"], 2
+            if st.session_state["superlinked_embeddings"] is not None:
+                st.write(
+                    f'Superlinked embeddings are created with shape: `{st.session_state["superlinked_embeddings"].shape}`'
                 )
-            st.write("Superlinked embedding PCA transformation ready.")
+
+        st.markdown("# Visualisation")
+        if st.session_state["naive_pca"] is None:
+            st.session_state["naive_pca"]: np.ndarray = pca_transform(
+                st.session_state["naive_embeddings"], 3
+            )
+        st.write("LLM embedding PCA transformation ready.")
+        if st.session_state["superlinked_pca"] is None:
+            st.session_state["superlinked_pca"]: np.ndarray = pca_transform(
+                st.session_state["superlinked_embeddings"], 3
+            )
+        st.write("Superlinked embedding PCA transformation ready.")
 
         if st.session_state["superlinked_pca"] is not None:
+            st.session_state["chart_dimension"] = st.selectbox(
+                "Chart dimensions", ["2d", "3d"]
+            )
             st.session_state["coloring_attribute"] = st.selectbox(
                 "Color by", st.session_state["df_to_use"].columns
             )
-            if st.session_state["coloring_attribute"] is not None:
+            if (st.session_state["coloring_attribute"] is not None) & (
+                st.session_state["chart_dimension"] is not None
+            ):
+                charter_function = (
+                    create_altair_2d_scatter
+                    if st.session_state["chart_dimension"] == "2d"
+                    else create_plotly_3d_scatter
+                )
                 if st.button("Create scatter plots!"):
                     column_1, column_2 = st.columns(2)
                     with column_1:
                         st.header("LLM embeddings")
-                        naive_chart = create_altair_2d_scatter(
+                        naive_chart = charter_function(
                             st.session_state["df_to_use"],
                             st.session_state["naive_pca"],
                             st.session_state["coloring_attribute"],
                             config,
                         )
-                        st.altair_chart(naive_chart)
+                        if st.session_state["chart_dimension"] == "2d":
+                            st.altair_chart(naive_chart)
+                        elif st.session_state["chart_dimension"] == "3d":
+                            st.plotly_chart(naive_chart)
+                        else:
+                            pass
 
                     with column_2:
                         st.header("Superlinked embeddings")
-                        superlinked_chart = create_altair_2d_scatter(
+                        superlinked_chart = charter_function(
                             st.session_state["df_to_use"],
                             st.session_state["superlinked_pca"],
                             st.session_state["coloring_attribute"],
                             config,
                         )
-                        st.altair_chart(superlinked_chart)
+                        if st.session_state["chart_dimension"] == "2d":
+                            st.altair_chart(superlinked_chart)
+                        elif st.session_state["chart_dimension"] == "3d":
+                            st.plotly_chart(superlinked_chart)
+                        else:
+                            pass
 
 
 if __name__ == "__main__":
--- util/visualisation.py
+++ util/visualisation.py
@@ -4,6 +4,7 @@ import pandas as pd
 import plotly_express as px
 
 from omegaconf import DictConfig
+from plotly.graph_objs import Figure
 
 
 def create_altair_2d_scatter(
@@ -12,7 +13,7 @@ def create_altair_2d_scatter(
     color_by_colname: str,
     config: DictConfig,
 ) -> alt.Chart:
-    chart_data = pd.DataFrame(component_vectors, columns=["Component 1", "Component 2"])
+    chart_data = pd.DataFrame(component_vectors[:, :2], columns=["Component 1", "Component 2"])
     chart_data[config.data.target_colname] = df[config.data.target_colname]
     if color_by_colname not in chart_data.columns:
         chart_data[color_by_colname] = df[color_by_colname]
@@ -39,5 +40,18 @@ def create_plotly_3d_scatter(
     component_vectors: np.ndarray,
     color_by_colname: str,
     config: DictConfig,
-):
-    pass
+) -> Figure:
+    chart_data = pd.DataFrame(
+        component_vectors, columns=["Component 1", "Component 2", "Component 3"]
+    )
+    chart_data[config.data.target_colname] = df[config.data.target_colname]
+    chart_data[color_by_colname] = df[color_by_colname]
+    return px.scatter_3d(
+        data_frame=chart_data,
+        x="Component 1",
+        y="Component 2",
+        z="Component 3",
+        color=color_by_colname,
+        width=config.chart.width,
+        height=config.chart.height,
+    )
